fd3f5f76403c9ac08d63f4aec47a39abdc91a6eb
Dika isnaida <dikaisnaida@students.amikom.ac.id>
2023-10-07 14:41:14 +0700

    landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+// text berganti
+let typingEffect = new Typed(".multiText", {
+  strings: ["Coders", "Students", "UI/UX Designer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+});
+
+
