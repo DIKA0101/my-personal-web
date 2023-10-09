466f7d3b5ea3a4cfcc50b2d2b4f256412c794477
Dika isnaida <dikaisnaida@students.amikom.ac.id>
2023-10-09 23:17:16 +0700

    penambahan hal about

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ let typingEffect = new Typed(".multiText", {
   backDelay: 1500,
 });
 
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabContents = document.getElementsByClassName("tab-contents");
 
+function opentab(tabname) {
+  for (tablink of tabLinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabContents) {
+    tabcontent.classList.remove("active-tab");
+  }
+ 
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
